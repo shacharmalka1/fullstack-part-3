3bd8373920a3dd1c8b90d0eb312764725697a3e9
17Amir17 <amirangel101@hotmail.com>
2021-11-07 14:26:14 +0200

    Documentation

--- src/routers/infoRouter.js
+++ src/routers/infoRouter.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 router.get('', (req, res, next) => {
   try {
-    const phonebookText = `Phonebook has info for ${db.persons.length} people <br/><br/>`;
+    // Send back amount of numbers in phonebook with curr date
+    const phonebookText = `Phonebook has info for ${db.persons.length} people <br/><br/>`; // <br/> for indentation
     const time = new Date();
     const response = phonebookText + time;
     res.send(response);

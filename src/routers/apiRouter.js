4271ca475ec930cc0362aa7e5999a5fbcb572366
17Amir17 <amirangel101@hotmail.com>
2021-11-07 14:10:27 +0200

    Added persons get command

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('', (req, res, next) => {
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+router.get('/persons', (req, res, next) => {
   try {
-    throw 'Not implemented';
+    res.json(persons);
   } catch (error) {
     next(error);
   }

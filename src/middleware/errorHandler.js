902a78faa0164b7120e7d5e5d61773a011b853bd
17Amir17 <amirangel101@hotmail.com>
2021-11-07 19:56:15 +0200

    Better error handling

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -2,22 +2,11 @@ const errorCodes = require('../constants/errorCodes');
 
 const errorHandler = (err, req, res, next) => {
   //Get error and check what type it is, send response to client
-  switch (err) {
-    case errorCodes.idParamInvalid:
-      res.status(400).json({ error: errorCodes.idParamInvalid });
-      break;
-    case errorCodes.personNotFound:
-      res.status(404).json({ error: errorCodes.personNotFound });
-      break;
-    case errorCodes.nameOrNumberMissing:
-      res.status(400).json({ error: errorCodes.nameOrNumberMissing });
-      break;
-    case errorCodes.nameMustBeUnique:
-      res.status(400).json({ error: errorCodes.nameMustBeUnique });
-      break;
-    default:
-      res.status(500).json({ error: err.message });
+  for (const error in errorCodes) {
+    if (error === err)
+      return res.status(error.code).json({ error: error.message });
   }
+  res.status(500).json({ error: err.message });
 };
 
 module.exports = errorHandler;

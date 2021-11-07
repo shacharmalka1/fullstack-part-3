2bb60488bb18bf6ee9a7f7abfc4b89821641a116
17Amir17 <amirangel101@hotmail.com>
2021-11-07 15:12:35 +0200

    Removed unnecessary try and catch

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -5,28 +5,20 @@ const router = express.Router();
 
 //Get request for all people
 router.get('/persons', (req, res, next) => {
-  try {
-    //Send persons in pretty way
-    res.header('Content-Type', 'application/json');
-    res.send(JSON.stringify(db.persons, null, 4));
-  } catch (error) {
-    next(error);
-  }
+  //Send persons in pretty way
+  res.header('Content-Type', 'application/json');
+  res.send(JSON.stringify(db.persons, null, 4));
 });
 
 //Get request for specific person
 router.get('/persons/:id', (req, res, next) => {
-  try {
-    //Get id param and check validity
-    const id = Number(req.params.id);
-    if (!id || typeof id != 'number') throw errorCodes.idParamInvalid;
-    const person = getPersonByID(id);
-    //Return found person in pretty way
-    res.header('Content-Type', 'application/json');
-    res.send(JSON.stringify(person, null, 4));
-  } catch (error) {
-    next(error);
-  }
+  //Get id param and check validity
+  const id = Number(req.params.id);
+  if (!id || typeof id != 'number') throw errorCodes.idParamInvalid;
+  const person = getPersonByID(id);
+  //Return found person in pretty way
+  res.header('Content-Type', 'application/json');
+  res.send(JSON.stringify(person, null, 4));
 });
 
 router.delete('/persons/:id', (req, res, next) => {
@@ -38,6 +30,8 @@ router.delete('/persons/:id', (req, res, next) => {
   db.persons = db.persons.filter((p) => p != person);
   res.send(`Person with id: ${id} succesfully deleted`);
 });
+
+router.post('/persons/', (req, res, next) => {});
 
 function getPersonByID(id) {
   //Find person with same id

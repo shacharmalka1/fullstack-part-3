7ea8aecc15f211ce6a8c7b5359ecb092663fda51
17Amir17 <amirangel101@hotmail.com>
2021-11-07 19:41:17 +0200

    linted

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,14 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "airbnb-base", "prettier",
-    ],
-    "parserOptions": {
-        "ecmaVersion": 13
-    },
-    "rules": {
-    }
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  rules: {
+    'javascript.suggestionActions.enabled': false,
+  },
 };

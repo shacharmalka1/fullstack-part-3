103a7ab4cabae6b155869c18e8f84ae5732a7551
17Amir17 <amirangel101@hotmail.com>
2021-11-08 14:19:10 +0200

    deploy

--- front/src/scripts/api/api.js
+++ front/src/scripts/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = 'http://localHost:3001';
+// const baseURL = 'http://localHost:3001';
+const baseURL = 'https://phonebookamir.herokuapp.com/';
 
 export async function getPersons() {
   try {

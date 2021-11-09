f83db55ef75b27e8e81243b5f5911702204d8aa5
17Amir17 <amirangel101@hotmail.com>
2021-11-09 13:56:23 +0200

    deploy

--- front/src/scripts/api/api.js
+++ front/src/scripts/api/api.js
@@ -1,52 +1,52 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const baseURL = 'http://localHost:3001';
-// const baseURL = 'https://phonebookamir.herokuapp.com';
+// const baseURL = 'http://localHost:3001';
+const baseURL = 'https://phonebookamir.herokuapp.com'
 
 const niceAlert = (textVal) => {
   Swal.fire({
     title: textVal,
     width: 600,
     padding: '3em',
-  });
-};
+  })
+}
 
 export async function getPersons() {
   try {
-    const res = axios.get(`${baseURL}/api/persons`);
-    return res;
+    const res = axios.get(`${baseURL}/api/persons`)
+    return res
   } catch (error) {
-    if (error.response) niceAlert(error.response.data.error);
-    else niceAlert(error);
+    if (error.response) niceAlert(error.response.data.error)
+    else niceAlert(error)
   }
 }
 
 export async function deletePerson(id) {
   try {
-    const res = axios.delete(`${baseURL}/api/persons/${id}`);
-    return res;
+    const res = axios.delete(`${baseURL}/api/persons/${id}`)
+    return res
   } catch (error) {
-    if (error.response) niceAlert(error.response.data.error);
-    else niceAlert(error);
+    if (error.response) niceAlert(error.response.data.error)
+    else niceAlert(error)
   }
 }
 
 export async function addPerson(name, number) {
   try {
-    const res = await axios.post(`${baseURL}/api/persons/`, { name, number });
-    return res;
+    const res = await axios.post(`${baseURL}/api/persons/`, { name, number })
+    return res
   } catch (error) {
-    if (error.response) niceAlert(error.response.data.error);
-    else niceAlert(error);
+    if (error.response) niceAlert(error.response.data.error)
+    else niceAlert(error)
   }
 }
 
 export async function editNumber(id, number) {
   try {
-    const res = await axios.put(`${baseURL}/api/persons/${id}`, { number });
-    return res;
+    const res = await axios.put(`${baseURL}/api/persons/${id}`, { number })
+    return res
   } catch (error) {
-    if (error.response) niceAlert(error.response.data.error);
-    else niceAlert(error);
+    if (error.response) niceAlert(error.response.data.error)
+    else niceAlert(error)
   }
 }
